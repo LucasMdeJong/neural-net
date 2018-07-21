04f1e8d2d20468dac7b833b920ba3412fd4fa3c5
LucasMdeJong <41298691+LucasMdeJong@users.noreply.github.com>
2018-07-21 16:11:27 +0200

    Update neuralNet.js

--- neuralNet.js
+++ neuralNet.js
@@ -13,14 +13,14 @@ class neuralNet{
 		this.hidn=hidNodes;
 		this.outn=outNodes;
 		this.lr=learnRate;
-		
+		this.weights=[];
 		let weightsarr=[];
 		let nodes=[inpNodes].concat(hidNodes,outNodes);
 		let u=0;
 		for(let k=0;k<nodes.length-1;k++){
 			weightsarr[k]=[];
 			for(let i=0;i<nodes[k+1];i++){
-				for(let j=0;j<hidNodes[k];j++){
+				for(let j=0;j<nodes[k];j++){
 					u=0;
 					while(u===0) u=Math.random()*2-1;
 					weightsarr[k].push(u);
@@ -39,7 +39,7 @@ class neuralNet{
 		let inp=[];
 		let out=[];
 		for(let k=0;k<this.hidn.length+2;k++){
-			if(k=0){
+			if(k===0){
 				inp[0]=inputM;
 				out[0]=inputM;
 				continue;
@@ -49,17 +49,16 @@ class neuralNet{
 		};
 		let err=[];
 		for(let k=this.hidn.length+1;k>=0;k--){
-			if(k=this.hidn.length+1) {
+			if(k===this.hidn.length+1) {
 				err[k]=matrix.substract(outputM,out[k]);
 				continue;
 			};
-			err[k]=matrix.dot(weights[k].transpose(),err[k+1]);
+			err[k]=matrix.dot(this.weights[k].transpose(),err[k+1]);
 		};
 		let update=0;
-		let lr=new matrix(1,1,[this.lr]);
 		for(let k=0;k<this.hidn.length+1;k++){
-			this.weights[k]=sum(this.weights[k],matrix.multiply(lr,matrix.dot(matrix.multiply(err[k+1],matrix.substract(out[k+1],matrix.multiply(out[k+1],out[k+1]))),out[k].transpose())));
-			//                   weight   +                      lr      *                    err*                              (Ok-Ok^2)                            *Oj^t       
+			this.weights[k]=matrix.sum(this.weights[k],matrix.scmultiply(this.lr,matrix.dot(matrix.multiply(err[k+1],matrix.substract(out[k+1],matrix.multiply(out[k+1],out[k+1]))),out[k].transpose())));
+			//                   weight   +                      lr      *                    err*                                       (Ok-Ok^2)                                           *Oj^t       
 		};
 		
 		
@@ -72,7 +71,7 @@ class neuralNet{
 		let inp=[];
 		let out=[];
 		for(let k=0;k<this.hidn.length+2;k++){
-			if(k=0){
+			if(k===0){
 				inp[0]=inputM;
 				out[0]=inputM;
 				continue;
